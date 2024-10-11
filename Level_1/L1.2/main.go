b843922d357b4ec58f1cdb8b40caeea61c45c761
alesande21 <via.rzv19@gmail.com>
2024-10-11 23:22:07 +0300

    done 1.2

--- Level_1/L1.2/main.go
+++ Level_1/L1.2/main.go
@@ -7,15 +7,21 @@ import (
 
 func main() {
 	arr := []int{2, 4, 6, 8, 10}
+	// создаем WaitGroup для ожидания завержения всех запущеных горутин
 	var wg sync.WaitGroup
 	for _, num := range arr {
+		// увеличиваем счетчик WaitGroup, так как будем запускать новую горутину
 		wg.Add(1)
+
+		// запускаем горутину
 		go func(num int) {
+			// уменьшаем счетчик WaitGroup после завершения выполнения горутины
 			defer wg.Done()
 			fmt.Println(num * num)
 		}(num)
 	}
 
+	// ждем пока завершаться все горутины
 	wg.Wait()
 
 }
